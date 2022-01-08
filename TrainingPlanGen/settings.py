3f30199f53b22431191e33ba1098358cd4602034
aceagles <ad.c.eagles@gmail.com>
2022-01-08 12:35:04 +0000

    setup bootstrap and bring in the basic login/logout functions

--- TrainingPlanGen/settings.py
+++ TrainingPlanGen/settings.py
@@ -38,6 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'plan_generator.apps.PlanGeneratorConfig',
+    'account.apps.AccountConfig',
+     'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -124,3 +127,5 @@ STATICFILES_DIRS = (os.path.join('static/'), )
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
