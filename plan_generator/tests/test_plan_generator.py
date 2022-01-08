e7a52c74afbd15d88c8efdf19c2409af057dfae0
aceagles <ad.c.eagles@gmail.com>
2022-01-08 14:29:13 +0000

    tests for week generator

--- plan_generator/tests/test_plan_generator.py
+++ plan_generator/tests/test_plan_generator.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+
 from plan_generator.methods import plan_generation
+
+
+def test_10_weeek():
+    startDate = datetime(2022, 1, 8)
+    endDate = datetime(2022, 3, 20)
+    output = [50, 55, 60, 33.0, 66, 72, 79, 43.0, 86, 94]
+
+    assert plan_generation.generate_weekly_totals(endDate, 100, 50, startDate) == output
