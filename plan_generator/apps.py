a19a697bfa17a74383dd390f4e28ec0d43d9a307
aceagles <ad.c.eagles@gmail.com>
2022-01-08 11:38:07 +0000

    recreate django source

--- plan_generator/apps.py
+++ plan_generator/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PlangeneratorsConfig(AppConfig):
+class PlanGeneratorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'planGenerators'
+    name = 'plan_generator'
