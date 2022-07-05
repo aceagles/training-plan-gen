45b41726c29df39259947e2780666e5782ec1db4
Adam Eagles <ad.c.eagles@gmail.com>
2022-07-05 20:37:36 +0100

    Add javascript and webpack for animated charts

---
+++ assets/javascript/App.jsx
@@ -0,0 +1,58 @@
+import React from 'react'
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+import {Line} from 'react-chartjs-2'
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
+export default function App(){
+    const labels = [
+        'January',
+        'February',
+        'March',
+        'April',
+        'May',
+        'June',
+      ];
+    
+      const data = {
+        labels: labels,
+        datasets: [{
+          label: 'My First dataset',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: [0, 10, 5, 2, 20, 30, 45],
+        }]
+      };
+    
+     
+
+
+    return (
+            <div>
+                
+<Line
+  options={{}}
+  data={data}
+  
+/>
+            </div>
+    )
+    
+}
